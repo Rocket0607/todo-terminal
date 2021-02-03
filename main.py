33790cba0d08b7ed4a3705b89ffd1ac020251059
Rocket0607 <konro@utschools.ca>
2021-02-02 21:59:59 -0500

    added input for project name

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import todoist
+
 api = todoist.TodoistAPI("9e76745d59cbda40acc52841cf097d3950c1bbe7")
-for project in api.items.all():
-    if project["project_id"] == 2233390427:
-        print(project["content"])
+project_name = input("project: ")
+project_id = None
+
+for project in api.state["projects"]:
+    if project["name"] == project_name:
+        project_id = project["id"]
+    if project_name == "":
+        project_id = 2233390427
+
+for item in api.items.all():
+    if item["project_id"] == project_id:
+        print(item["content"])
